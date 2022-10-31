326b9f6af49951422c9789b99e5450693b99708f
q1347878001 <113610481+q1347878001@users.noreply.github.com>
2022-10-31 23:06:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -82,7 +82,7 @@ export const config = {
         {"type": "节日", "name": "恋爱纪念日", "year": "2018", "date": "11-05"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "生日", "name": "靖靖", "year": "2000", "date": "12-22"},
-        {"type": "节日", "name": "下次见面", "year": "2022", "date": "10-01"},
+        {"type": "节日", "name": "下次见面", "year": "2023", "date": "01-22"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
